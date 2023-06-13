4d509a52889bab3f4737bf0e7301928c28cffb73
Alexander <yusovsky2@gmail.com>
2023-06-13 22:03:42 +0300

    fixed base url

--- src/redux/axiosHerokuInstance.js
+++ src/redux/axiosHerokuInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https:connections-api.herokuapp.com',
+  baseURL: 'https://connections-api.herokuapp.com',
 });
 
 export const pushToken = token => {
