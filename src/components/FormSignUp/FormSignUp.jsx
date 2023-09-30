e3075a0f8a1c8a63eb0d42e835c058a0f8bc6597
Alexander <yusovsky2@gmail.com>
2023-09-30 19:52:28 +0300

    add formik

--- src/components/FormSignUp/FormSignUp.jsx
+++ src/components/FormSignUp/FormSignUp.jsx
@@ -1,15 +1,30 @@
 import css from './FormSignUp.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
 import { signUpThunk } from 'redux/authService/thunks';
 import { setError } from 'redux/authService/authSlice';
 import { authSelector } from 'redux/stateSelectors';
 
-const FormSignUp = () => {
-  const [name, setName] = useState(''); // DemoUser0
-  const [email, setEmail] = useState(''); // DemoUser0@mail.com
-  const [password, setPassword] = useState(''); // DemoUser0
+const signupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Enter Your name, please!'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Enter Your email, please!'),
+  password: Yup.string()
+    .min(7)
+    .required('Enter Your password, please!')
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{4,}$/,
+      'At least: one uppercase letter, one lowercase letter, one number, one special symbol'
+    ),
+});
 
+const FormSignUp = () => {
   const { error } = useSelector(authSelector);
   const dispatch = useDispatch();
 
@@ -18,8 +33,7 @@ const FormSignUp = () => {
     dispatch(setError(null));
   }, [error, dispatch]);
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleSubmit = ({ name, email, password }, { resetForm }) => {
     dispatch(
       signUpThunk({
         name,
@@ -27,58 +41,94 @@ const FormSignUp = () => {
         password,
       })
     );
+    resetForm();
+    console.log('hi');
   };
 
   return (
-    <form onSubmit={handleSubmit} className={css.authForm} autoComplete="off">
-      <label className={css.formLabel}>
-        <input
-          type="text"
-          name="name"
-          title="Enter Your Name, please!"
-          required
-          value={name}
-          onChange={e => {
-            setName(e.target.value);
-          }}
-          className={css.input}
-          placeholder="Name"
-        />
-      </label>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={signupSchema}
+      onSubmit={handleSubmit}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        setFieldTouched,
+        isValid,
+        handleSubmit,
+      }) => (
+        <form
+          onSubmit={handleSubmit}
+          className={css.authForm}
+          autoComplete="off"
+        >
+          <label className={css.formLabel}>
+            <Field
+              name="name"
+              title="Enter Your Name, please!"
+              required
+              className={css.input}
+              placeholder="Name"
+              value={values.name}
+              onChangeText={handleChange('name')}
+              onBlur={() => setFieldTouched('name')}
+            />
+            {errors.name && touched.name && (
+              <p style={{ margin: 4 }} className={css.errorText}>
+                {errors.name}
+              </p>
+            )}
+          </label>
 
-      <label className={css.formLabel}>
-        <input
-          type="text"
-          name="email"
-          title="Enter email, please!"
-          required
-          value={email}
-          onChange={e => {
-            setEmail(e.target.value);
-          }}
-          className={css.input}
-          placeholder="email"
-        />
-      </label>
-      <label className={css.formLabel}>
-        <input
-          type="tel"
-          name="password"
-          title="Enter password, please!"
-          required
-          value={password}
-          onChange={e => {
-            setPassword(e.target.value);
-          }}
-          className={css.input}
-          placeholder="password"
-        />
-      </label>
+          <label className={css.formLabel}>
+            <Field
+              name="email"
+              title="Enter email, please!"
+              required
+              className={css.input}
+              placeholder="email"
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={() => setFieldTouched('email')}
+            />
+            {errors.email && touched.email && (
+              <p style={{ margin: 4 }} className={css.errorText}>
+                {errors.email}
+              </p>
+            )}
+          </label>
+          <label className={css.formLabel}>
+            <Field
+              name="password"
+              title="Enter password, please!"
+              required
+              className={css.input}
+              placeholder="password"
+              value={values.password}
+              onChangeText={handleChange('password')}
+              onBlur={() => setFieldTouched('password')}
+            />
+            {errors.password && touched.password && (
+              <p style={{ margin: 4 }} className={css.errorText}>
+                {errors.password}
+              </p>
+            )}
+          </label>
 
-      <button className={css.authButton} type="submit">
-        {'SignUp'}
-      </button>
-    </form>
+          {/* стили менять тернарником */}
+          <button className={css.authButton} type="submit" disabled={!isValid}>
+            {'SignUp'}
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
